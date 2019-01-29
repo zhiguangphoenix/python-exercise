a0196f14fb3844026135de91ea1bf8cf83d917fb
chenzhiguang <chenzhiguang@bytedance.com>
2019-01-29 17:31:17 +0800

    django tutorial finish

--- django/demosite/polls/admin.py
+++ django/demosite/polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,      {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question)
